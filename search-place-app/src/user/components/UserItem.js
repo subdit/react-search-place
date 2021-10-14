33cc6c7be383d5d8f5610a18998acc247f9069a1
Kasidit Bualoeng <subdit@gmail.com>
2021-10-14 16:26:23 -0400

    modified UserItem,UsersList components

--- search-place-app/src/user/components/UserItem.js
+++ search-place-app/src/user/components/UserItem.js
@@ -1,21 +1,27 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
 import "./UserItem.css"
+import Card from "../../shared/components/UIElements/Card"
 import Avatar from "../../shared/components/UIElements/Avatar"
 
 const UserItem = props => {
   return (
     <li className='user-item'>
-      <div className='user-item_content'>
-        <div className='user-item_image'>
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className='user-item_info'>
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className='user-item_content'>
+        {/*  use string literal for passing the value */}
+        <Link to={`/${props.id}/places`}>
+          <div className='user-item_image'>
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className='user-item_info'>
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   )
 }
--- search-place-app/src/user/pages/Users.js
+++ search-place-app/src/user/pages/Users.js
@@ -6,10 +6,10 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Sub Dit",
+      name: "Subdit Bualoeng",
       image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3
+        "https://media.istockphoto.com/photos/aerial-view-of-kualoa-area-of-oahu-hawaii-picture-id938335974?b=1&k=20&m=938335974&s=170667a&w=0&h=dVrv6pCmMpICvTikPT0GygUsDZxl4dLbr_aT_JnSZf8=",
+      places: 1
     }
   ]
 
