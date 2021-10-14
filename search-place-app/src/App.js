12ad418959f32ff3df00997b431f8da936c070a5
Kasidit Bualoeng <subdit@gmail.com>
2021-10-14 09:53:45 -0400

    modified app.js file

--- search-place-app/src/App.js
+++ search-place-app/src/App.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Users from "./user/pages/Users"
 
 function App() {
-  return <h2>Shall we begin!</h2>
+  return (
+    <Router>
+      <Route path='/'>
+        <Users />
+      </Route>
+    </Router>
+  )
 }
 
 export default App
