e93e70574305e3f8c39f335f7408db26a8c9963c
Kasidit Bualoeng <subdit@gmail.com>
2021-10-15 08:41:42 -0400

    add Navigation/MainHeader, MainNavigation components

---
+++ search-place-app/src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import "./MainHeader"
+
+const MainHeader = props => {
+  return <header className='main-header'>{props.children}</header>
+}
+
+export default MainHeader
--- search-place-app/src/user/pages/Users.js
+++ search-place-app/src/user/pages/Users.js
@@ -8,7 +8,7 @@ const Users = () => {
       id: "u1",
       name: "Sub Bualoeng",
       image:
-        "https://media.istockphoto.com/photos/aerial-view-of-kualoa-area-of-oahu-hawaii-picture-id938335974?b=1&k=20&m=938335974&s=170667a&w=0&h=dVrv6pCmMpICvTikPT0GygUsDZxl4dLbr_aT_JnSZf8=",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Prathatphanom_03.jpg/1280px-Prathatphanom_03.jpg",
       places: 3
     }
   ]
